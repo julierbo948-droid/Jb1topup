4bfb5c13669bef6eea410a53956d2a88a6f5525c
julierbo948-droid <julierbo948@gmail.com>
2026-04-10 23:47:49 +0700

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -738,16 +738,18 @@ async def auto_calculator(message: types.Message):
             # Telegram API 7.0+ ရဲ့ တိုက်ရိုက် Copy ကူးပေးတဲ့ Feature
             from aiogram.types import CopyTextButton
             copy_btn = InlineKeyboardButton(
-                text=" ᴄᴏᴘʏ ", 
+                text=" ᴄᴏᴘʏ: {formatted_result} ", 
                 copy_text=CopyTextButton(text=full_copy_text),
-                style="danger" # အရောင်ပါအောင် style ထည့်ခြင်း
+                style="danger" 
+                icon_custom_emoji_id="5456498809875995940"# အရောင်ပါအောင် style ထည့်ခြင်း
             )
         except ImportError:
             # အပေါ်က method အလုပ်မလုပ်ရင် switch_inline သုံးမယ်
             copy_btn = InlineKeyboardButton(
-                text=" ᴄᴏᴘʏ ", 
+                text=" ᴄᴏᴘʏ: {formatted_result} ", 
                 switch_inline_query_current_chat=full_copy_text,
                 style="danger"
+                icon_custom_emoji_id="5456498809875995940"
             )
 
         keyboard = InlineKeyboardMarkup(inline_keyboard=[[copy_btn]])
